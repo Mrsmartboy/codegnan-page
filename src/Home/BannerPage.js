ac0a6e04c8bc2cad419d4f3645a11f6bfa25f759
Mrsmartboy <josephganjela@gmail.com>
2024-12-22 11:02:33 +0530

    some updates

--- src/Home/BannerPage.js
+++ src/Home/BannerPage.js
@@ -1,69 +1,77 @@
+import React, { useState } from 'react';
+import './BannerPage.css';
+import StatsChart from './StatsChart';
 
-import React from 'react';
-// import bannerGirl from '../images/banner-girl.webp';
-import './BannerPage.css'
-import StatsChart from './StatsChart'
-// import ReactPlayer from 'react-player';
+const BannerPage = () => {
+    const [isPlayerVisible, setIsPlayerVisible] = useState(false);
 
-const BannerPage =()=>{
-return(
-  
-       
-<div className='coverpage-container'>
-<div className='home-cover-text-container'>
-  <div className='home-text-container'>
-    <div className='home-titles'>
-    <p className='home-title'>
-      It's <span className='span-home-title'>Not Just</span> A Numbers
-    </p>
-    <p className='tag-line'>
-      See Successful Students <span className='span-home-title'>Placements</span> Journey
-    </p>
-    </div>
-    
-  <div className="placement-card">
-  <h1 className="student-count">
-    3407<span className="plus-sign ">+</span>
+    const handleVideoLoad = () => {
+        setIsPlayerVisible(true);
+    };
 
-  </h1>
-  <p className="students-placed">Students Placed</p>
+    const videoId = "uTBlsOoiMQs"; // Extracted YouTube video ID
 
+    return (
+        <div className='coverpage-container'>
+            <div className='home-cover-text-container'>
+                <div className='home-text-container'>
+                    <div className='home-titles'>
+                        <p className='home-title'>
+                            It's <span className='span-home-title'>Not Just</span> A Numbers
+                        </p>
+                        <p className='tag-line'>
+                            See Successful Students <span className='span-home-title'>Placements</span> Journey
+                        </p>
+                    </div>
 
-  <p className="counting"><span className='blinking'>&gt;&gt;&gt; Still Counting...!</span></p>   
-    </div>
-      
-  </div>
-  <div className='stats-studentplaced-container'>
-    
+                    <div className="placement-card">
+                        <h1 className="student-count">
+                            3407<span className="plus-sign ">+</span>
+                        </h1>
+                        <p className="students-placed">Students Placed</p>
+                        <p className="counting">
+                            <span className='blinking'>&gt;&gt;&gt; Still Counting...!</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='stats-studentplaced-container'>
+                    <StatsChart />
 
-     <StatsChart />
+                    {/* Lazy-loaded YouTube video */}
+                    <div className="video-wrapper" style={{ width: '400px', height: '225px' }}>
+                        {!isPlayerVisible ? (
+                            <img
+                                src="https://res.cloudinary.com/db2bpf0xw/image/upload/v1734774565/youtube_s19iwj.png"
+                                alt="YouTube Thumbnail"
+                                className="youtube-thumbnail"
+                                style={{ width: '100%', height: '100%', cursor: 'pointer' }}
+                                onClick={handleVideoLoad}
+                            />
+                        ) : (
+                            <iframe
+                                width="400"
+                                height="225"
+                                src={`https://www.youtube.com/embed/${videoId}`}
+                                title="YouTube video"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        )}
+                    </div>
+                </div>
+            </div>
 
-     {/* <div className="video-wrapper" style={{ width: '400px', height: '225px' }}>
-      <ReactPlayer
-        url="https://youtu.be/uTBlsOoiMQs?si=ppEqo_c6IuI53lKK"
-        width="100%"
-        height="100%"
-        className="custom-player"
-        controls={true} 
-        
-      />
-    </div> */}
+            <div className='image-container'>
+                <img
+                    src="https://res.cloudinary.com/db2bpf0xw/image/upload/v1734774536/banner-girl_w3yoxr.png"
+                    alt="Banner Girl"
+                    className="banner-girl"
+                    loading="lazy"
+                />
+            </div>
+        </div>
+    );
+};
 
-  </div>
-</div>
-
-<div className='image-container'>
-<img src="https://res.cloudinary.com/db2bpf0xw/image/upload/v1734774536/banner-girl_w3yoxr.png"
-    alt="Banner Girl" 
-    className="banner-girl" 
-    loading="lazy"
-   
-    />
-</div>
-</div>
-  
-    )
-}
-
-export default BannerPage
-
+export default BannerPage;
