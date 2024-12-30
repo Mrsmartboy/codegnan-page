bb8207a7a8f208e5cf6876596079ca2f60539ec5
Mrsmartboy <josephganjela@gmail.com>
2024-12-30 16:52:52 +0530

    some updates

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,10 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    '@fullhuman/postcss-purgecss': {
+      content: ['./src/**/*.{html,js,jsx,ts,tsx}'], 
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: [], 
+    },
   },
 };
