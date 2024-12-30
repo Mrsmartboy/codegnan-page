e70c792b9167de672ca4d0335c2c582317f844d1
Mrsmartboy <josephganjela@gmail.com>
2024-12-30 12:52:30 +0530

    some updates

--- src/Home/BannerPage.js
+++ src/Home/BannerPage.js
@@ -67,7 +67,6 @@ const BannerPage = () => {
                 height="720"
                 style={{ width: "100%", height: "100%", cursor: "pointer" }}
                 onClick={handleVideoLoad}
-                loading="lazy"
               />
             ) : (
               <iframe
